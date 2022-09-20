aa1004656f83d11a0545caaac70b922e3a5d418e
chanpolo <chanpolo.cuisine@gmail.com>
2022-09-20 23:19:08 +0200

    json js

---
+++ content/recette/poulet-roti.md
@@ -0,0 +1,71 @@
+---
+author: "Paul"
+title: "Poulet Rôti"
+titleshort:
+date: 2022-09-18T08:16:11+02:00
+draft: false
+
+description: >
+    Une recette simple de poulet rôti. Mais délicieuse, avec une volaille à la peau croustillante sans être brûlée.
+    C'est une recette de *[Chef Simon](https://chefsimon.com/gourmets/chef-simon/recettes/poulet-roti-la-bonne-cuisson)*, dont les conseils sont toujour précieux.
+
+short: >
+    Une recette simple, mais une volaille parfaitement réussie
+    
+keywords: ""
+
+categories: ["entrée", "plat", "dessert", "base"]
+tags: []
+
+photo: "poulet-roti.jpg"
+alt: "sur une planche à découper, un beau poulet fermier rôti, doré à point ; sur le devant de la photo, un couteau à découper"
+caption: ""
+source: >
+    [Poulet Rôti : la bonne cuisson](https://chefsimon.com/gourmets/chef-simon/recettes/poulet-roti-la-bonne-cuisson)
+
+remarque: >
+    Pour 4 personnes<br>
+    Une recette de *[Chef Simon](https://chefsimon.com/gourmets/chef-simon/recettes/poulet-roti-la-bonne-cuisson)*<br>
+    On peut débuter la cuisson à froid en enfournant la volaille au moment où on allume le four
+
+preparation: 
+cuisson: 
+difficulte:
+---
+
+
+## Ingrédients
+- Un poulet fermier
+- Sel, poivre
+- Beurre fondu
+- Herbes aromatiques
+## Préparation
+### Avant Cuisson
+- Vérifier la volaille : éliminer si nécessaire les duvets subsistants
+- Possiblement vous pourrez saler et poivrer de l’intérieur et y ajouter quelques branches de thym
+- *[Brider le poulet](https://chefsimon.com/articles/lexique-brider)*
+- Placer le poulet prêt à cuire dans un plat à four de taille adaptée ou une plaque à rôtir ; éventuellement le placer sur un panier à rôtir
+- Badigeonner la peau avec un peu d'huile neutre (arachide, tournesol..) ou de beurre fondu
+
+### Cuisson
+- Placer au four froid ou préchauffé à 170°C pendant environ 1h30 (le temps de cuisson dépend de la qualité de la volaille)
+- **Inutile de retourner ou arroser le poulet en cours de cuisson**
+- À l’issue de la cuisson piquer entre la cuisse et l’aile et basculer le poulet
+- Un jus limpide et clair devrait s’y échapper, c’est le signe que la volaille est juste cuite
+- Enlever la ficelle de bridage
+- Replacer au four monté à 200°C quelques minutes si on le préfère bien doré
+- Débarrasser le poulet dans un autre plat et le réserver au four tempéré à 70°C juste pour le garder chaud
+### Préparation du jus
+- Dégraisser
+- Chauffer les sucs à feu doux (directement dans la plaque à rôtir ou dans une casserole)
+- Déglacer avec 5 dl d’eau
+- Réduire et passer au chinois
+- Dégraisser si nécessaire et réduire à nouveau pour obtenir un jus corsé
+- Le jus est un concentré on en a donc besoin de peu
+- On peut se simplifier la vie en récupérant juste le jus de cuisson et en le faisant réduire si nécessaire
+### Dressage
+- *[Découper la volaille](https://chefsimon.com/gourmets/chef-simon/recettes/decouper-une-volaille-rotie)*
+- Servir sur un plat avec le jus à part
+
+
+
---
+++ static/js/suggestions.js
@@ -0,0 +1,79 @@
+
+  const date = new Date();
+  const month = date.getUTCMonth()+1;
+  let day = date.getUTCDate();
+  
+  const year = date.getUTCFullYear();
+  const number = day + month + year;
+
+  async function getEntrees() {
+    let url = "json/entree.json"
+    let res = await fetch(url);
+    return await res.json();
+  }  
+
+  async function getPlats() {
+    let url = "json/plat.json"
+    let res = await fetch(url);
+    return await res.json();
+  } 
+  async function getDesserts() {
+    let url = "json/dessert.json"
+    let res = await fetch(url);
+    return await res.json();
+  }
+
+  async function renderSuggestions() {
+    const entrees = await getEntrees();
+    const plats = await getPlats();
+    const desserts = await getDesserts();
+   
+    const index_entree = number % entrees.length; 
+    const index_plat = number % plats.length
+    const index_dessert = number % desserts.length;
+
+    const index_entree2 = (index_entree + Math.floor(index_entree / 2)) % index_entree;
+    const index_plat2 = (index_plat + Math.floor(index_plat / 2)) % index_plat;
+
+    const entree = entrees[index_entree];
+    const entree2 = entrees[index_entree2];
+    const plat = plats[index_plat];
+    const plat2 = plats[index_plat2];
+    const dessert = desserts[index_dessert];
+
+    const suggestions = [entree, entree2, plat, plat2, dessert]
+
+    let html = '';
+
+      suggestions.forEach(suggestion => {
+        let htmlSegment = `
+                          <li class="hidden lg:inline-grid grid grid-cols-6  gap-x-4 pt-1">
+                          <a class="col-span-1" href="${suggestion.url}">
+                            <img src="${suggestion.image}" alt="${suggestion.alt}">
+                          </a>
+                          <a class="col-span-2 capitalize md:tracking-wide no-underline" href="${suggestion.url}">
+                          <span class="font-normal md:font-semibold md:text-xl md:pt-1">${suggestion.Title}</span>
+                          <br><span class="md:text-lg">${suggestion.date}</span>
+                          <br><span class="md:text-lg">${suggestion.author}</span>
+                          </a>
+                          <span class="col-span-3 tracking-tighter md:tracking-wide lg:tracking-widest md:text-lg">${suggestion.description}</span>
+                          </li>
+                          <li class="lg:hidden grid grid-cols-6  gap-2 text-xs pt-1">
+                          <a class="col-span-1" href="${suggestion.url}">
+                            <img src="${suggestion.image}" alt="${suggestion.alt}">
+                          </a>
+                          <a class="col-span-2 capitalize no-underline" href="${suggestion.url}">
+                          <span class="">${suggestion.titleshort}</span>
+                          <br><span class="">${suggestion.date}</span>
+                          <br><span class="">${suggestion.author}</span>
+                          </a>
+                          <span class="col-span-3 ">${suggestion.short}</span>
+                          </li>
+                          `;
+        html += htmlSegment;
+      });
+      document.getElementById("suggestions").innerHTML = html;
+    }
+
+  
+  renderSuggestions()
