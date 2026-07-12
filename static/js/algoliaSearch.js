8e40ae339950749c4f4031f3a008e2a6b420a129
chanpolo <chanpolo.cuisine@gmail.com>
2026-07-12 18:03:47 +0200

    algolia

--- static/js/algoliaSearch.js
+++ static/js/algoliaSearch.js
@@ -4,5 +4,5 @@ algoliasearchNetlify({
     siteId: 'c441371c-c790-4a40-993c-bdf9cd973932',
     branch: 'main',
     selector: 'div#search',
-    indexName: 'chanpolo_2026-07',
+    indexName: 'search-20260711',
     })
